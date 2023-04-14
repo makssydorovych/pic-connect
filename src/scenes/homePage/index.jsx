7b6d518a2aa67a9add73579bbe5a48e39e5d9c06
Maksym <maksymsydorovych@gmail.com>
2023-04-14 22:46:54 +0200

    login layout

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Box from "@mui/material/Box";
+import Navbar from "../navbar";
 
 const HomePage = () => {
-    return <div>HomePage</div>
+    return <Box>
+        <Navbar/>
+    </Box>
 };
 
 export default HomePage;
--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import {useMediaQuery, useTheme} from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const LoginPage = () => {
-    return <div>LoginPage</div>
+    const theme = useTheme();
+    const isMobileScreens = useMediaQuery("(min-width: 1000px)");
+    return <Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+            <Typography
+                fontWeight="bold"
+                fontSize="32px"
+                color="primary"
+            >PIC-CONNECT
+            </Typography>
+        </Box>
+        <Box
+            width={isMobileScreens ? "50%" : "93%"}
+            p="2rem"
+            m="2rem auto"
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" variant="h5" sx={{mb: "1.5rem"}}>
+                Welcome to PIC-CONNECT, social media for share pictures.
+            </Typography>
+        </Box>
+    </Box>
 };
 
 export default LoginPage;
