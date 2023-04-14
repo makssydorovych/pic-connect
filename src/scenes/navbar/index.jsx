705d9f96832a4c9798e8c0f7d0aa0fdf95f76b1d
Maksym <maksymsydorovych@gmail.com>
2023-04-14 21:51:21 +0200

    form control in navbar, select

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -73,6 +73,29 @@ const Navbar = () => {
                         <LightMode sx={{color: dark, fontSize: "25px"}}/>
                     )}
                 </IconButton>
+            <Message sx={{fontSize: "25px"}}/>
+            <Notifications sx={{fontSize: "25px"}}/>
+            <Help sx={{fontSize: "25px"}}/>
+            <FormControl variant="standard" value={fullName}>
+                <Select value={fullName}
+                sx={{
+                    backgroundColor: neutralLight,
+                    width: "150px",
+                    borderRadius: "0.25rem",
+                    p: "0.25rem 1rem",
+                    "& .MuiSvgIcon-root":{
+                        pr:"0.25rem",
+                        width: "3rem"
+                    },
+                    "& .MuiSelect-select: focus":{
+                        backgroundColor: neutralLight
+                    }
+                }}
+                        input = {<InputBase/>}
+                >
+                    <MenuItem value={fullName}></MenuItem>
+                </Select>
+            </FormControl>
             </FlexBetween>)
             : (<IconButton></IconButton>)}
     </FlexBetween>
