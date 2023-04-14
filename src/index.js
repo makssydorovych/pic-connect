7d166d568b2fa7c4791d0ced14fb0e316b2160f2
Maksym <maksymsydorovych@gmail.com>
2023-04-14 21:11:43 +0200

    add mode, theme useSelectors, CssBaseline

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, R
 import storage from "redux-persist/lib/storage";
 import {PersistGate} from "redux-persist/integration/react";
 
+
 const persistConfig = {key: "root", storage, version: 1};
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
