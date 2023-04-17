657825d4a9e93dd1a515b3ce92382a80fcd64298
Maksym <maksymsydorovych@gmail.com>
2023-04-17 13:29:49 +0200

    add layout for widgets

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -2,11 +2,31 @@ import React from 'react';
 import Box from "@mui/material/Box";
 import Navbar from "../navbar";
 import {useMediaQuery} from "@mui/material";
+import {useSelector} from "react-redux";
+import UserWidget from "../widgets/UserWidget";
 
 const HomePage = () => {
-    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const { _id, picturePath} = useSelector((state)=>state.user);
     return <Box>
         <Navbar/>
+        <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens? "flex":"block"}
+        gap="0.5rem"
+        justigyContent="space-between"
+        >
+<Box flexBasis={isNonMobileScreens? "26%": undefined}><UserWidget userId={_id} picturePath={picturePath}/></Box>
+            <Box flexBasis={isNonMobileScreens? "42%" : undefined}
+            mt={isNonMobileScreens? undefined: "2rem"}
+            ></Box>
+            {isNonMobileScreens &&(
+                <Box flexBasis="26%">
+
+                </Box>
+            )}
+        </Box>
     </Box>
 };
 
