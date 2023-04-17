0a6e9a67655c5b8974e16496e2df278eea6eafa2
Maksym <maksymsydorovych@gmail.com>
2023-04-17 13:23:16 +0200

    ADD user widget (left section)

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Box from "@mui/material/Box";
 import Navbar from "../navbar";
+import {useMediaQuery} from "@mui/material";
 
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
     return <Box>
         <Navbar/>
     </Box>
